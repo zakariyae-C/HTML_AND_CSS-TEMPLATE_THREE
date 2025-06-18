c2092bc398336f9a0e6cc8fbee168f3827337d10
Zakariyae <175839414+zakariyae-C@users.noreply.github.com>
2025-06-18 14:30:12 +0100

    I created a login and signup pop-up and enabled interaction between them. Show pop-up automatically after page load. Click outside to close this pop-up.

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+const loginFormBtn = document.getElementById("login-form-open"),
+  signupFormBtn = document.getElementById("signup-form-open"),
+  loginSignup = document.querySelector(".login-signup"),
+  formContainer = document.querySelector(".form-container"),
+  formCloseBtn = document.querySelector(".form-close"),
+  signupBtn = document.getElementById("signup"),
+  loginBtn = document.getElementById("login"),
+  pwShowHide = document.querySelectorAll(".pw-hide");
+/* Stat Show login automaticaly */
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    if (loginSignup.classList.length === 1) {
+      loginSignup.classList.add("show-login");
+      document.body.style.overflow = "hidden";
+    }
+  }, 2000);
+});
+/* End Show login automaticaly */
+loginFormBtn.addEventListener("click", () => {
+  if (loginSignup.classList.length === 1) {
+    loginSignup.classList.add("show-login");
+    document.body.style.overflow = "hidden";
+  }
+});
+signupFormBtn.addEventListener("click", () => {
+  if (loginSignup.classList.length === 1) {
+    loginSignup.classList.add("show-signup");
+    document.body.style.overflow = "hidden";
+  }
+});
+formCloseBtn.addEventListener("click", () => {
+  loginSignup.classList.remove("show-login", "show-signup");
+  document.body.style.overflow = "";
+});
+
+loginSignup.addEventListener("click", (event) => {
+  if (event.target === loginSignup) {
+    loginSignup.classList.remove("show-login", "show-signup");
+    document.body.style.overflow = "";
+  }
+});
+
+signupBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+/* Start pwShowHide */
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("fa-eye-slash", "fa-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("fa-eye", "fa-eye-slash");
+    }
+  });
+});
+/* End pwShowHide */
+
+/* Start Show nav burgerMenu */
+document.querySelector(".burger-menu").addEventListener("click", () => {
+  document.querySelector(".burger-menu").classList.toggle("active");
+});
+/* End Show nav burgerMenu */
